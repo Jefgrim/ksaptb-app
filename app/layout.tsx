6bac08a268291f74a589b7c7dc590ad6cffd9fcb
Jefgrim <jefgrim@gmail.com>
2025-12-20 07:48:51 +0800

    toaster fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import ConvexClientProvider from "@/components/ConvexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          {children}
+          
+          <Toaster /> 
+          
+        </ConvexClientProvider>
       </body>
     </html>
   );
